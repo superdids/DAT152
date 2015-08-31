e4d3cee515e800c757997ba35b447f60c55f4fc2
Didrik Aubert <didrik.aubert@gmail.com>
2015-08-31 11:31:08 +0200

    Created project containing notes for lectures..

--- DigitalRain/WebContent/js/digitalRain.js
+++ DigitalRain/WebContent/js/digitalRain.js
@@ -1,57 +1,84 @@
 "use strict";
 
-var symbols = generateSymbols();
-var rows = 15;
-var cols = 75;
-var counter = generateCounters();
+var canvas, ctx, fontSize, cols;
+//var symbols = generateSymbols();
+var symbols = "あかさたないきしちにうくすつぬえけせてねおこそとのはまやらわひみりをふむゆるんへめれ".split("");
+
+var drops = [];
+var toDim = [];
+var count = [];
+var len = 20;
+function draw()
+{
+	for(var x = 0; x < drops.length; x++) {
+		if(rand(200) > 198 || drops[x] > 0) {
+			toDim[x][drops[x]]  = symbols[rand(symbols.length)];
+
+			if(drops[x] - len >= 0) {
+				ctx.fillStyle = "black";
+				ctx.clearRect(x*fontSize, (drops[x]-len)*fontSize, fontSize, fontSize);
+				
+			} 
+			ctx.fillStyle = "#01DF3A";
+			for(var y = 1; y < len; y++) {
+				ctx.fillText(toDim[x][drops[x]-y], x*fontSize, (drops[x]-y)*fontSize);
+			}
+			
+			ctx.fillStyle = "#071907";
+			for(var y = len; y < drops[x]; y++) {
+				ctx.fillText(toDim[x][drops[x]-y], x*fontSize, (drops[x]-y)*fontSize);
+			}
+			
+			ctx.fillStyle = "#FFFFFF";
+			ctx.fillText(toDim[x][drops[x]] , x*fontSize, drops[x]*fontSize);
+			
+			if(drops[x]*fontSize > canvas.height && rand(1000) > 975) {
+				drops[x] = 0;
+			}
+			
+			
+			
+
+			drops[x]++;
+		}
+	}
+}
+
+//window.onresize = function() {
+//canvas = document.getElementById('c');
+//canvas.width = screen.width;
+//canvas.height= screen.height;
+//ctx = canvas.getContext("2d");
+//fontSize = 20;
+//cols = canvas.width/fontSize;
+//}
 
 window.onload = function() {
-	var elm = document.getElementById('digital');
-	elm.rows = rows;
-	elm.cols = cols;
+	canvas = document.getElementById('c');
+	canvas.width = screen.width;
+	canvas.height= screen.height;
 
-	window.setInterval(function() {
-		elm.value = "";
-		for(var x = 0; x < rand(10); x++) {
-			elm.value += symbols[rand(symbols.length)];
-		}
-	}, 100);
-	
+	ctx = canvas.getContext("2d");
+	fontSize = 20;
+	cols = canvas.width/fontSize;
+	for(var x = 0; x < cols; x++) {
+		drops[x] = 0; 
+		toDim[x] = new Array();
+		count[x] = 0;
+	}
+	ctx.font = fontSize + "px arial";
+	window.setInterval(draw, 50);
 }
+
+
+
 
 
 function rand(arg) {
 	return Math.floor(Math.random() * arg);
 }
 
-
-
-
 function generateSymbols() {
-	var symb = new Array();
-	var index = 0;
-	for(; index < 10; index++) {
-		symb[index] = index;
-	}
-	
-	symb[index] = "@"; index++;
-	symb[index] = "#"; index++;
-	symb[index] = "$"; index++;
-	symb[index] = "{"; index++;
-	symb[index] = "}"; index++;
-	symb[index] = "["; index++;
-	symb[index] = "]"; index++;
-	symb[index] = "="; index++;
-	
-	return symb;
-}
-
-function generateCounters() {
-	var arr = new Array();
-	for(var x = 0; x < cols; x++) {
-		arr[x] = new Array();
-		for(var y = 0; y < rows; y++) {
-			arr[x][y] = 8;
-		}
-	}
+	var arr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "@", "#", "$", "%", "&", "{", "}", "[", "]", "="];
+	return arr;
 }
